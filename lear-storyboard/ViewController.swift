dd27a8501b69a6495abe27af86f3871e7c5ad948
Nicolas Posa <nicoposa57@gmail.com>
2024-08-28 00:11:59 -0300

    validaciones con código

--- lear-storyboard/ViewController.swift
+++ lear-storyboard/ViewController.swift
@@ -14,6 +14,19 @@ class ViewController: UIViewController {
     
     
     @IBAction func loginButtonaction() {
+        //obtener valores de los texfield
+        let email = emailTextField.text
+        let password = passwordTextField.text
+        
+        if email == "nico@gmail.com", password == "123"{
+            print("Bienvenido!")
+            performSegue(withIdentifier: "home_segue", sender: nil)
+        } else {
+            print("Credenciales invalidas")
+        }
+    
+        
+        
     }
     
     override func viewDidLoad() {
